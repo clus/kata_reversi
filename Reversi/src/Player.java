d93c426535f248bebd85ff10d0242cc83129f8bd
Clément <Clment@10.7.225.148>
2013-11-12 10:18:42 +0100

    1er commit

---
+++ Reversi/src/Player.java
@@ -0,0 +1,14 @@
+
+public class Player {
+	
+	private String couleur;
+
+	public String getCouleur() {
+		return couleur;
+	}
+
+	public void setCouleur(String couleur) {
+		this.couleur = couleur;
+	}
+
+}
---
+++ Reversi/src/Reversi.java
@@ -0,0 +1,27 @@
+
+public class Reversi {
+
+	private Player blackPlayer = new Player();
+	private Player whitePlayer = new Player();
+	
+	public void afficherGrid(){
+		private String[] grid= {
+				"........",
+				"........",
+				"........",
+				"...BW...",
+				"...WB...",
+				"........",
+				"........",
+				"........"};
+	}
+	
+	
+	public Reversi(){
+		blackPlayer.setCouleur("B");
+		whitePlayer.setCouleur("W");
+					
+	}
+	
+	
+}
